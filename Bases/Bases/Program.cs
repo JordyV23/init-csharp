363053d0873c85a630999286ba2561ae68708f69
JordyV23 <jordyvargas0123@gmail.com>
2023-10-12 10:52:11 -0600

    Tipado, constantes y palabras reservadas

--- Bases/Bases/Program.cs
+++ Bases/Bases/Program.cs
@@ -6,7 +6,42 @@ namespace Bases
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*
+             Este es 
+                un comentario 
+                        multilinea
+             */
+
+            //Este es un comentario en linea
+
+            string nombre = "Jordy";
+
+            //Con var se asignan valores implicitos
+            var apellido = "Vargas";
+
+            //Tipados
+            int entero = 100;
+
+            //----
+            //Los valores para numeros no enteros, se pueden usar los siguientes
+            //Los float deben de llevar la f
+            float flotante = 36.23f;
+            double doubles = 36.94;
+            //Los deciman deben de llevar la m
+            decimal valorDecima = 450.567m;
+            //----
+
+            //Tiene como maximo 255
+            byte myLittleNumber = 255;
+
+            bool isLearning = true;
+
+            char caracter = 'J';
+
+
+            const int constante = 10;
+
+            Console.WriteLine("Hello World! Atte: " + nombre );
         }
     }
 }
